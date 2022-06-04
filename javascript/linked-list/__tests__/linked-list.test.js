f3826f9a8de0f6a24d8f6b0dcf9b12cc5cea8d68
WajeehHussein <wajehessam3@gmail.com>
2022-06-04 22:49:59 +0300

    linked

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,56 @@
 'use strict';
 
-// Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedLilst = require('../lib/linkedlist');
 
 describe('Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
   });
+  test(' an empty linked list', () => {
+    let list = new LinkedLilst();
+    expect(list.head).toBeNull()
+  })
+  test('insert into the linked list', () => {
+    let list = new LinkedLilst();
+    list.insert("value")
+    expect(list.head.value).toEqual("value")
+  })
+  test('the head the first node in the linked list', () => {
+    let list = new LinkedLilst();
+    list.insert("value")
+    list.insert("one")
+    list.insert("two")
+    list.insert("three")
+    expect(list.head.value).toEqual("value")
+  })
+  test('insert multiple nodes into the linked list', () => {
+    let list = new LinkedLilst();
+    list.insert(1)
+    list.insert(2)
+
+    expect(list.insert(3)).toEqual({ "head": { "next": { "next": { "next": null, "value": 3 }, "value": 2 }, "value": 1 } })
+  })
+  test(' finding a value within the linked list that exists', () => {
+    let list = new LinkedLilst();
+    list.insert(1)
+    list.insert(2)
+
+    expect(list.includes(2)).toEqual(true)
+  })
+  test.skip('searching for a value in the linked list that does not exist', () => {
+    let list = new LinkedLilst();
+    list.insert(1)
+    list.insert(2)
+
+    expect(list.includes(3)).toEqual(true)
+  })
+  test('insert multiple nodes into the linked list', () => {
+    let list = new LinkedLilst();
+    list.insert(1)
+    list.insert(2)
+    list.insert(3)
+
+    expect(list).toEqual({ "head": { "next": { "next": { "next": null, "value": 3 }, "value": 2 }, "value": 1 } })
+  })
+
 });
---
+++ javascript/linked-list/lib/linkedlist.js
@@ -0,0 +1,47 @@
+'use strict';
+const Node = require('./node')
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode
+            return this;
+        } else {
+            let currentNode = this.head;
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode
+            return this;
+        }
+    }
+
+
+    includes(value) {
+        let currentNode = this.head
+        while (currentNode) {
+            if (value === currentNode.value) {
+                return true
+            }
+            currentNode = currentNode.next;
+        }
+        return false
+    }
+
+    toString() {
+        let currentNode = this.head;
+        while (currentNode.next) {
+            currentNode = currentNode.next;
+        }
+        return `${this.head} -> ${this.currentNode}`;
+    }
+
+}
+
+
+module.exports = LinkedList;
