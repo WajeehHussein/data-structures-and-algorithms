041ae9fddcde67b6420f7ec2cbc3634a12faedb3
WajeehHussein <wajehessam3@gmail.com>
2022-06-17 21:52:06 +0300

    i

--- javascript/code-challenges/linked-list-zip/lib/linkedlist.js
+++ javascript/code-challenges/linked-list-zip/lib/linkedlist.js
@@ -28,58 +28,28 @@ class LinkedList1 {
             currentNode = currentNode.next;
         }
     }
-}
 
-class LinkedList2 {
-    constructor() {
-        this.head = null
-    }
+    merge(L1, L2) {
+        let lNext1, lNext2
+        this.head = L1.head
+        let currentNode = this.head
+        // this.head = L1
+        while (L1 != null && L2 != null) {
+            lNext1 = L1.next;
+            L1.next = L2
+            L1 = lNext1
 
-    append(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            return this
-        } else {
-            let currentNode = this.head;
-            while (currentNode.next) {
-                currentNode = currentNode.next
-            }
-            currentNode.next = newNode
-            return this
+            lNext2 = L2.next;
+            L2.next = lNext1
+            L2 = lNext2
+            currentNode = currentNode.next
         }
-    }
-
-    printList() {
-        let currentNode = this.head;
-        while (currentNode != null) {
-            console.log(`{ ${currentNode.value} } ->`);
-            currentNode = currentNode.next;
-        }
-    }
-}
-
-class ZipLinkedList {
-    constructor() {
-        this.head = null
-    }
-
-    printList() {
-        let currentNode = this.head;
-        while (currentNode != null) {
-            console.log(`{ ${currentNode.value} } ->`);
-            currentNode = currentNode.next;
-        }
+        return currentNode
     }
 }
 
 
-module.exports = {
-    LinkedList1,
-    LinkedList2,
-    ZipLinkedList
-
-}
+module.exports = LinkedList1
 
 
 
