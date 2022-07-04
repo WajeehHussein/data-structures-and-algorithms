9f45ba1a4367e393a45671b8ec752309816dab35
WajeehHussein <wajehessam3@gmail.com>
2022-07-04 15:41:36 +0300

    code 15

--- javascript/tree/index.js
+++ javascript/tree/index.js
@@ -1,8 +1,9 @@
 'use strict';
 // const BinaryTree = require('./binary-tree');
 const BinarySearchTree = require('./binary-search-tree');
-const newBST = new BinarySearchTree();
+// const BST = new BinarySearchTree();
 const Node = require('./Node');
+const treeify = require("treeify")
 
 // let tree = null;
 
@@ -40,14 +41,29 @@ const Node = require('./Node');
 // console.log('post order: ', postOrder);
 //pre order >> 8 , 9 , 7 , 6 , 2 , 4 , 5 , 3 , 1
 
-newBST.insert(15);
-newBST.insert(25);
-newBST.insert(10);
-newBST.insert(7);
-newBST.insert(22);
-newBST.insert(17);
-newBST.insert(13);
-newBST.insert(5);
-newBST.insert(9);
-newBST.insert(27);
-console.log(newBST.contains(5));
+// let tree2=new BTS(one);
+// tree.contains(1);
+// tree2.add(10);
+// console.log(tree.preOrder());//pre order > 1 , 2 , 6 , 7 , 8 , 9 , 3 , 4 , 5
+// console.log(tree.inOrder());
+// console.log(tree.postOrder());//pre order > 8 , 9 , 7 , 6 , 2 , 4 , 5 , 3 , 1
+
+let one = new Node(4);
+let two = new Node(2);
+let three = new Node(5);
+let four = new Node(1);
+let five = new Node(3);
+let six = new Node(6);
+
+let tree2 = new BinarySearchTree(one);
+one.left = two;
+one.right = three;
+two.left = four;
+two.right = five;
+three.right = six;
+
+// console.log({ tree2 });
+tree2.add(20)
+console.log(treeify.asTree(tree2, true))
+console.log(tree2.contains(50));
+console.log(tree2.contains(5));
