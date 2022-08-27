862282979fca877c627741572331d551c2239894
WajeehHussein <wajehessam3@gmail.com>
2022-08-28 00:41:44 +0300

    32

---
+++ javascript/tree/hashTable.js
@@ -0,0 +1,149 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    append(value) {
+        const node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
+    }
+
+
+    values() {
+        let values = [];
+        let current = this.head;
+        while (current) {
+            values.push(current.value);
+            current = current.next;
+        }
+        return values;
+    }
+}
+
+class Hashmap {
+    constructor(size) {
+        this.size = size;
+        this.map = new Array(size);
+    }
+    hash(key) {
+        const asciicodeSum = key.split("").reduce((acc, cur) => {
+            return acc + cur.charCodeAt(0);
+        }, 0);
+        const multiPrime = asciicodeSum * 599;
+        const theIndex = multiPrime % this.size;
+        // console.log(theIndex)
+        return theIndex;
+    }
+    set(key, value) {
+        const hash = this.hash(key);
+
+        if (!this.map[hash]) {
+            this.map[hash] = new LinkedList();
+        }
+        this.map[hash].append({ [key]: value });
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        let value;
+        if (this.map[index]) {
+            let current = this.map[index].head;
+
+            while (current) {
+                if (current.value[key]) value = current.value[key];
+                current = current.next;
+                // console.log("....", value);
+
+            }
+        }
+        return value;
+    }
+
+    contain(key) {
+        const index = this.hash(key);
+        let bolleanValue = false;
+
+        if (this.map[index]) {
+            // console.log(this.map[index].head);
+            let current = this.map[index].head;
+
+            while (current) { // if I have a collision
+                if (Object.keys(current.value) == key) {
+                    return bolleanValue = true;
+                }
+                current = current.next;
+            }
+        }
+        return bolleanValue;
+    }
+
+    keys() {
+        let keys = [];
+
+        this.map.map(e => {
+            let current = e.head;
+            while (current) {
+
+                keys.push(Object.keys(current.value))
+                current = current.next;
+            }
+        });
+
+
+        return keys;
+    }
+
+    repeated(str) {
+
+        let arr = str.toLowerCase().split(" ");
+        // console.log(arr);
+        let hash = new Hashmap();
+        let isHere = false;
+        let result;
+        for (let i = 0; i < arr.length; i++) {
+            isHere = hash.contain(arr[i]);
+            hash.set(arr[i]);
+            result = arr[i];
+            // console.log(result);
+
+            if (isHere) break;
+        }
+        return result;
+    }
+
+}
+
+const myhashmap = new Hashmap();
+myhashmap.set('a', 'AAAAAA');
+myhashmap.set('b', 'BBBBBBB');
+// console.log(myhashmap.repeated('a b c d a a '));
+// myhashmap.set('c', 'CCCCCCC');
+// myhashmap.set('d', 'DDDDDDD');
+// myhashmap.set('e', 'EEEEEEE');
+// myhashmap.set('f', 'FFFFFFF');
+// myhashmap.set('c', 'CCCCCC');
+
+// console.log(myhashmap.map[8]);
+// console.log(myhashmap.map[8].head.next);
+
+// console.log(myhashmap);
+// myhashmap.map.forEach((ll) => {
+//     console.log(ll.values());
+// })
+// console.log(myhashmap.hash('a'));
+
+module.exports = Hashmap;
--- javascript/tree/index.js
+++ javascript/tree/index.js
@@ -1,105 +1,65 @@
 'use strict';
-// const BinaryTree = require('./binary-tree');
-const BinarySearchTree = require('./binary-search-tree');
+const BinaryTree = require('./binary-tree');
+// const BinarySearchTree = require('./binary-search-tree');
 // const BST = new BinarySearchTree();
 const Node = require('./Node');
-const treeify = require("treeify")
+// const treeify = require("treeify");
+const treeIntersection = require('./treeIntersection');
+// const Hashmap = require('./hashTable')
 
-// let tree = null;
-
-// let one = new Node(1);
-// let two = new Node(2);
-// let three = new Node(3);
-// let four = new Node(4);
-// let five = new Node(5);
-// let six = new Node(6);
-// let seven = new Node(7);
-// let eight = new Node(8);
-// let nine = new Node(9);
-
-// one.left = two;
-// one.right = three;
-// two.left = six;
-// six.right = seven;
-// seven.left = eight;
-// seven.right = nine;
-// three.left = four;
-// three.right = five;
-
-// tree = new BinaryTree(one);
-
-// let preOrder = tree.preOrder();
-// console.log('pre order: ', preOrder);
-// //pre order >> 1 , 2 , 6 , 7 , 8 , 9 , 3 , 4 , 5
-
-// let inOrder = tree.inOrder();
-// console.log('in order: ', inOrder);
-// //pre order >> 6 , 8 , 7 , 9 , 2 , 1 , 4 , 3 , 5
-
-
-// let postOrder = tree.postOrder();
-// console.log('post order: ', postOrder);
-//pre order >> 8 , 9 , 7 , 6 , 2 , 4 , 5 , 3 , 1
-
-// let binaryTree=new BTS(one);
-// tree.contains(1);
-// tree2.add(10);
-// console.log(tree.preOrder());//pre order > 1 , 2 , 6 , 7 , 8 , 9 , 3 , 4 , 5
-// console.log(tree.inOrder());
-// console.log(tree.postOrder());//pre order > 8 , 9 , 7 , 6 , 2 , 4 , 5 , 3 , 1
 
 let one = new Node(4);
 let two = new Node(2);
 let three = new Node(5);
 let four = new Node(1);
 let five = new Node(3);
-let six = new Node(6);
 
-let f1 = new Node("folder");
-let f2 = new Node(".js");
-let f3 = new Node(".sql");
-let f4 = new Node("folder");
-let f5 = new Node(".java");
-let f6 = new Node("folder");
+let f1 = new Node(1);
+let f2 = new Node(2);
+let f3 = new Node(3);
+let f4 = new Node(4);
+let f5 = new Node(6);
+// let f6 = new Node("folder");
 
-let f10 = new Node("folder");
-let f20 = new Node(".js");
-let f30 = new Node(".sql");
-let f40 = new Node("folder");
-let f50 = new Node(".java");
-let f60 = new Node("folder");
+// let f10 = new Node("folder");
+// let f20 = new Node(".js");
+// let f30 = new Node(".sql");
+// let f40 = new Node("folder");
+// let f50 = new Node(".java");
+// let f60 = new Node("folder");
 
-let binaryTree = new BinarySearchTree(one);
 one.left = two;
 one.right = three;
 two.left = four;
 two.right = five;
-three.right = six;
+let binaryTree1 = new BinaryTree(one);
 
-let folderTree1 = new BinarySearchTree(f1);
 f1.left = f2;
 f1.right = f3;
 f2.left = f4;
 f2.right = f5;
-f3.right = f6;
+// f3.right = f6;
+let binaryTree2 = new BinaryTree(f1);
 
-let folderTree2 = new BinarySearchTree(f10);
-f10.left = f20;
-f10.right = f30;
-f20.left = f40;
-f20.right = f50;
-f30.right = f60;
+treeIntersection(binaryTree1, binaryTree2)
+
+// let folderTree2 = new BinarySearchTree(f10);
+// f10.left = f20;
+// f10.right = f30;
+// f20.left = f40;
+// f20.right = f50;
+// f30.right = f60;
 
 // console.log({ binaryTree });
 // binaryTree.add(20)
 // console.log(treeify.asTree(binaryTree, true))
-console.log(binaryTree.contains(50));
-console.log(binaryTree.contains(5));
-console.log('/////max', binaryTree.findMax());
-console.log(binaryTree.breadthFirst());
-console.log("$$$$$$$", binaryTree.sumOfAllOdd());
-console.log('///////////////////////////////////////////////////////////////////////////////////////');
-console.log("fooooolder", folderTree1.compareDirectories(folderTree1, folderTree2));
-console.log(folderTree1.treeAsArray());
+// console.log(binaryTree.contains(50));
+// console.log(binaryTree.contains(5));
+// console.log('/////max', binaryTree.findMax());
+// console.log(binaryTree.breadthFirst());
+// console.log("$$$$$$$", binaryTree.sumOfAllOdd());
+// console.log('///////////////////////////////////////////////////////////////////////////////////////');
+// console.log("fooooolder", folderTree1.compareDirectories(folderTree1, folderTree2));
+// console.log(folderTree1.treeAsArray());
 
 // console.log("/********************", folderTree1.isSameStructure(folderTree1, folderTree1));
